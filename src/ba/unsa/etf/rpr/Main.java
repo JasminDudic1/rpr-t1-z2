ea514a8575ff9c68b7460e4216a7cff952501b11
Jasmin <jasabarsa@yahoo.com>
2019-10-13 18:02:21 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.rpr;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static int sumaCifara(int n){
+
+        int suma=0;
+
+        while(n>0){
+            suma+=n%10;
+            n/=10;
+        }
+
+    return suma;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Unesite broj n");
+        int n=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for (int i = 0; i <n ; i++) {
+            System.out.println(sumaCifara(i));
+
+        }
+
+    }
+}
